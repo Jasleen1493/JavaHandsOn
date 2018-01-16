5aff8919f72d1825acc344602124643b33969120
Jasleen <14.jasleen.93@gmail.com>
2018-01-16 16:58:23 +0530

    unit testing

---
+++ src/test/java/com/thinking/in/java/ClassesObjectsTest.java
@@ -0,0 +1,21 @@
+
+package com.thinking.in.java;
+
+import com.thinking.in.java.classes.objects.ClassesObjects;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassesObjectsTest {
+
+
+    /**
+     * done inlining in the method - ALT + SHIFT + I
+     */
+    @Test
+    public void testGetterSetter() {
+        new ClassesObjects().setStr("Hello");
+        assertEquals("Hello", new ClassesObjects().getStr());
+    }
+
+}
